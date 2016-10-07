34f83dce91af4740306f663843bfbbd3668a8b69
moeyui1 <894immyk@gmail.com>
2016-10-07 22:38:25 +0800

    修改说明文字

--- static/app.js
+++ static/app.js
@@ -248,7 +248,7 @@ class TableFrame extends React.Component {
                     </div>
                     <div className=" panel-body">
                         <p style={{'font-size': '15px'}}>
-                            以下智库列表是经过各省市宣传部理论处、哲社规划办、教育部直属高校社科处等有关机构的推荐，最终整理形成的514家智库名单。请您在仔细阅览后，至少选出50家您认为
+                            以下列表是在各省市宣传部理论处、哲社规划办、教育部直属高校社科处等有关机构推荐智库名单基础上，最终整理形成的513家智库。请您在仔细阅览后，至少选出50家您认为
                             <strong className="warning-text">不属于</strong>
                             智库的机构（即被否决的智库数量大于等于50），并对所选智库投<strong className="warning-text">否决票</strong>（勾选即可）。
                             <p>
