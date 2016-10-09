62aaeee954f2b724a30701143e02b93195a5fe6e
moeyui1 <894immyk@gmail.com>
2016-10-09 16:13:51 +0800

    修改说明文字

--- static/app.js
+++ static/app.js
@@ -248,9 +248,9 @@ class TableFrame extends React.Component {
                     </div>
                     <div className=" panel-body">
                         <p style={{'font-size': '15px'}}>
-                            以下列表是在各省市宣传部理论处、哲社规划办、教育部直属高校社科处等有关机构推荐智库名单基础上，最终整理形成的513家智库。请您在仔细阅览后，至少从中选出50家您认为
-                            <strong className="warning-text">不属于</strong>
-                            智库的机构（即被否决的智库数量大于等于50），并对所选智库投<strong className="warning-text">否决票</strong>（勾选即可）。
+                            以下列表是在各省市宣传部理论处、哲社规划办、教育部直属高校社科处等有关机构推荐智库名单基础上，最终整理形成的513家智库。请您在仔细阅览后，至少
+                            <strong className="warning-text">否决</strong>
+                            50家智库（即被否决的智库数量大于或等于50），并对所选智库投<strong className="warning-text">否决票</strong>（勾选即可）。
                             <p>
                                <strong style={{'text-decoration':'underline'}}>
                                    需要注意：投票过程需一次性完成，投票完成后请务必点击“提交”按钮进行提交，否则投票结果视为无效。您一旦选择提交，则无法进行二次投票。
@@ -261,7 +261,7 @@ class TableFrame extends React.Component {
                 <div id="table-panel">
 
                     <div className="panel panel-default">
-                        <div className="panel-heading">智库列表</div>
+                        <div className="panel-heading"><h4>智库列表</h4></div>
                         <table className=" tableizer-table table table-striped table-hover table-responsive">
                             <thead>
                             <tr className=" tableizer-firstrow">
